8cb6e19d98c71456512a14a079f884229e417006
ComandorChe <fordima2@yandex.ru>
2021-04-13 23:27:14 +0300

    4.1.2. Глупый бот [#454829]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        if (question.equals("Привет Бот.")) {
+            return "Привет умник.";
+        } else if (question.equals("Пока.")) {
+            return "До скорой встречи.";
+        } else {
+            return "Это ставит меня в тупик. Задайте другой вопрос.";
+        }
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Сколько будет 2 + 2?");
+        System.out.println(rsl);
+    }
+}
